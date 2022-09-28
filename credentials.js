ad594fadb8a3dc44a1a5b9ff84f3712e5c120700
jblackman42 <jblackman@pureheart.org>
2022-09-28 11:17:36 -0700

    filters and accessable styling

--- credentials.js
+++ credentials.js
@@ -1,9 +1,10 @@
+//CONTENT INFORMATION
 const apiKey = '12345678';   // <-------- put api key here in quotes - 'API_KEY'
 const locations = [
     {
         latLng: {lat: 33.614769, lng: -112.152802},
         title: "Pure Heart Church",
-        specialties: ['Addiction', 'Anxiety/Depression', 'Family', 'Marriage', 'Trauma'],
+        specialties: ['Addiction'],
         phone: '(602) 866-8850',
         email: 'info@pureheart.org',
         addressLine: '14240 N 43rd Ave',
@@ -11,5 +12,50 @@ const locations = [
         state: 'AZ',
         zip: '85306',
         website: 'https://www.pureheart.org'
+    },
+    {
+        latLng: {lat: 33.614769, lng: -112.152802},
+        title: "Pure Heart Church",
+        specialties: ['Anxiety/Depression', 'cell phone repair'],
+        phone: '(602) 866-8850',
+        email: 'info@pureheart.org',
+        addressLine: '14240 N 43rd Ave',
+        city: 'Phoenix',
+        state: 'AZ',
+        zip: '85306',
+        website: 'https://www.pureheart.org'
+    },
+    {
+        latLng: {lat: 33.614769, lng: -112.152802},
+        title: "Pure Heart Church",
+        specialties: ['Family'],
+        phone: '(602) 866-8850',
+        email: 'info@pureheart.org',
+        addressLine: '14240 N 43rd Ave',
+        city: 'Glendale',
+        state: 'AZ',
+        zip: '85306',
+        website: 'https://www.pureheart.org'
+    },
+    {
+        latLng: {lat: 33.614769, lng: -112.152802},
+        title: "Pure Heart Church",
+        specialties: ['Family', 'Marriage', 'Trauma'],
+        phone: '(602) 866-8850',
+        email: 'info@pureheart.org',
+        addressLine: '14240 N 43rd Ave',
+        city: 'Chandler',
+        state: 'AZ',
+        zip: '85306',
+        website: 'https://www.pureheart.org'
     }
 ]
+//STYLING INFORMATION
+const widgetWidth = '1000px';
+const widgetPrimaryTextColor = '#000000'; //black
+const locationBackgroundColor = '#eee';//light gray
+const locationBorderColor = '#ccc'; //gray
+const locationsTextColor = '#000000'; //black
+const locationsButttonColor = '#13aa52'; //green
+const locationsButttonTextColor = '#fff'; //white
+const locationsLinkColor = '#3498db' //blue
--- main.js
+++ main.js
@@ -5,24 +5,38 @@ script.async = true;
 
 let map;
 // Attach your callback function to the `window` object
-window.initMap = function() {
+window.initMap = function(filteredLocations) {
   // JS API is loaded and available
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 33.614977, lng: -112.152791 },
     zoom: 10,
+    mapTypeControl: false,
+    streetViewControl: false
   });
 
+  //if filtered locations are passed in use those for pins instead of the list of all locations
+  const mapLocations = filteredLocations ? filteredLocations : locations;
+
   const infoWindow = new google.maps.InfoWindow();
-  //load all 
-  for (let i = 0; i < locations.length; i ++) {
-    const {latLng, title} = locations[i];
+  //load all pins on the map
+  for (let i = 0; i < mapLocations.length; i ++) {
+    const {latLng, title, specialties} = mapLocations[i];
+
+    const titleElem = `
+        <div class="marker-title">
+            <h1>${title}</h1>
+            <div class="marker-info">
+                <p class="marker-info-label">Specialties:</p>
+                <p>${specialties.map(specialty => specialty).join(', ')}</p>
+            </div>
+        </div>
+    `
 
     const marker = new google.maps.Marker({
         position: latLng,
         map,
-        title: title,
-        label: `${i + 1}`,
-        animation: google.maps.Animation.DROP
+        title: titleElem,
+        label: `${i + 1}`
     })
     // Add a click listener for each marker, and set up the info window.
     marker.addListener("click", () => {
@@ -36,33 +50,122 @@ window.initMap = function() {
 // Append the 'script' element to 'head'
 document.head.appendChild(script);
 
+//funcion which zooms the map on the point of the location when you use the 'veiw on map' button
+const zoomOnPoint = (lat, lng) => {
+    map.setCenter({lat: lat, lng: lng})
+    map.setZoom(18)
+    // map.setCenter(new GLatLng(lat, lon), );
+}
+
+//function to list all the locations so it can be updated by running the function
 const locationsListDOM = document.getElementById('locations-list');
-locationsListDOM.innerHTML = locations.map(location => {
-    const {title, specialties, phone, email, addressLine, city, state, zip, website} = location;
+const loadLocationsList = (locations) => {
+
+    locationsListDOM.innerHTML = locations.map((location, i) => {
+        const {latLng, title, specialties, phone, email, addressLine, city, state, zip, website} = location;
+        return `
+            <li class="location-container">
+                <div class="header">
+                    <h1>${i + 1}. ${title}</h1>
+                    <button class="btn" onclick="zoomOnPoint(${latLng.lat}, ${latLng.lng})">View On Map</button>
+                </div>
+                <div class="info-container">
+                    <p class="label">Specialties</p>
+                    <p>${specialties.map(specialty => specialty).join(', ')}</p>
+                </div>
+                <div class="info-container">
+                    <p class="label">Contact Phone</p>
+                    <a class="link" href="tel:${phone}">${phone}</a>
+                </div>
+                <div class="info-container">
+                    <p class="label">Contact Email</p>
+                    <a class="link" href="mailto:${email}">${email}</a>
+                </div>
+                <div class="info-container">
+                    <p class="label">Address</p>
+                    <p>${addressLine}</p>
+                    <p>${city}, ${state} ${zip}</p>
+                </div>
+                <div class="info-container">
+                    <p class="label">Website</p>
+                    <a class="link" href="${website}">${website}</a>
+                </div>
+            </li>
+        `
+    }).join('')
+}
+loadLocationsList(locations);
+
+//get cities and specialties from locations and make them filter options in the select dropdown menu
+const citySelectDOM = document.getElementById('city-select');
+const specialtySelectDOM = document.getElementById('specialty-select');
+
+const allCities = [...new Set(locations.map(location => location.city))]
+let allSpecialties = [];
+locations.forEach(location => allSpecialties = [... new Set(allSpecialties.concat(location.specialties))])
+
+const citySelectHTML = allCities.map(city => {
     return `
-        <li>
-            <h1>${title}</h1>
-            <div class="info-container">
-                <p class="label">Specialties</p>
-                <p>${specialties.map(specialty => specialty).join(', ')}</p>
-            </div>
-            <div class="info-container">
-                <p class="label">Contact Phone</p>
-                <a href="tel:${phone}">${phone}</a>
-            </div>
-            <div class="info-container">
-                <p class="label">Contact Email</p>
-                <a href="mailto:${email}">${email}</a>
-            </div>
-            <div class="info-container">
-                <p class="label">Address</p>
-                <p>${addressLine}</p>
-                <p>${city}, ${state} ${zip}</p>
-            </div>
-            <div class="info-container">
-                <p class="label">Website</p>
-                <a href="${website}">${website}</a>
-            </div>
-        </li>
+<option value="${city}">${city}</option>
     `
-}).join('')
+})
+citySelectHTML.unshift(`<option value="0">All</option>`)
+citySelectDOM.innerHTML = citySelectHTML.join('')
+
+const specialtySelectHTML = allSpecialties.map(specialty => {
+    return `
+<option value="${specialty}">${specialty}</option>
+    `
+})
+specialtySelectHTML.unshift(`<option value="0">All</option>`)
+specialtySelectDOM.innerHTML = specialtySelectHTML.join('')
+
+//runs on the change of the select inputs
+//edits the list of locations, changes the pins on the map to match the list below
+const setFilter = () => {
+    const city = citySelectDOM.value;
+    const specialty = specialtySelectDOM.value;
+    
+    let filteredLocations;
+
+    if (city == 0 && specialty == 0) {
+        //no filters
+        filteredLocations = locations;
+    } else if (city == 0 && specialty) {
+        //only specialty filter
+        filteredLocations = locations.filter(location => location.specialties.includes(specialty));
+    } else if (city && specialty == 0) {
+        //only city filter
+        filteredLocations = locations.filter(location => location.city == city);
+    } else {
+        //both city and specialty filter
+        filteredLocations = locations.filter(location => location.city == city && location.specialties.includes(specialty));
+    }
+
+    loadLocationsList(filteredLocations)
+    initMap(filteredLocations)
+}
+
+//SETTING COLORS FROM credentials.js
+const counselingWidget = document.getElementById('counseling-widget');
+counselingWidget.style.width = widgetWidth;
+counselingWidget.style.color = widgetPrimaryTextColor;
+
+const locationsDOM = document.querySelectorAll('.location-container');
+locationsDOM.forEach(elem => {
+    elem.style.backgroundColor = locationBackgroundColor;
+    elem.style.borderColor = locationBorderColor;
+    elem.style.color = locationsTextColor;
+})
+
+const locationsLinksDOM = document.querySelectorAll('.link');
+locationsLinksDOM.forEach(elem => {
+    elem.style.color = locationsLinkColor;
+})
+
+const locationsButtonsDOM = document.querySelectorAll('.btn');
+locationsButtonsDOM.forEach(elem => {
+    elem.style.backgroundColor = locationsButttonColor;
+    elem.style.borderColor = locationsButttonColor;
+    elem.style.color = locationsButttonTextColor;
+})
